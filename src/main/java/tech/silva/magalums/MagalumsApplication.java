7dc3d2f014ff8f2e8f83829389c90c6793a850e3
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-09-23 11:50:33 -0300

    feat: create the project and connecting with docker mysql

---
+++ src/main/java/tech/silva/magalums/MagalumsApplication.java
@@ -0,0 +1,13 @@
+package tech.silva.magalums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagalumsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagalumsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/silva/magalums/MagalumsApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.silva.magalums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagalumsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
